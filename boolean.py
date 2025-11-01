df338808306c60511d52df529aa0056446e2d5f8
HellMind-azx <nostalgiabook176@gmail.com>
2025-11-01 13:21:09 +0500

    all code add to file

---
+++ boolean.py
@@ -0,0 +1,70 @@
+# ===================================================
+# THEME: Boolean
+# ===================================================
+
+#numbers = '153429786'
+#sum_ = 0
+#max_ = 0
+#second_max = 0
+
+#for letter in numbers:
+#    num = int(letter)
+#    sum_ += num
+#    if num > max_:
+#        max_ = num
+#
+#
+#print('The sum is: ', sum_)
+#print('The max is: ', max_)
+#print('The second max is: ', second_max)
+
+#temperature = float(input('Enter the temperature: '))
+#cold = 15.5
+#hot = 28
+#
+#if temperature <= cold:
+#    print('ХОЛОДНО')
+#elif temperature >= hot:
+#    print('ЖАРКО')
+#else:
+#    print('НОРМАЛЬНО')
+
+#password = input()
+#confirm = input()
+#
+#if len(password) < 8:
+#    print('Korotko')
+#elif password != confirm:
+#    print('Razlichayutsya')
+#else:
+#    print('OK') 
+
+#while True:
+#    n = int(input())
+#    if n == 0:
+#        break
+#    print(n)
+#
+
+#         || 
+#         || 
+#        \  /
+#         \/
+
+
+#n = int(input())
+#while n != 0:
+#    print(n)
+#    n = int(input())
+
+#numbers = []
+#
+#while True:
+#    n = int(input())
+#    if n == 0:
+#        break
+#    numbers.append(n)
+#
+#for num in numbers:
+#    print('...')
+#    print(num)
---
+++ cikls.py
@@ -0,0 +1,91 @@
+# x = 10
+# while x > 0:
+#    print('Counter: ' + str(x))
+#    x -= 1
+#
+# print('Vse koncheno!')
+
+# number = int(input('Vvedite chislo: '))
+# while number > 0:
+#    print('Вы ввели положительное число: ' + str(number))
+#    number = int(input())
+#    print('Введите следующее число: ')
+#    break
+# print('Вы ввели отрицательное число или ноль.')
+
+# count = 0
+# number = int(input('write a number: '))
+# while number % 2 != 0:
+#    number = int(input('Write an even number: '))
+#    if number % 2 == 0:
+#        print('You write an even number , bro!')
+#        break
+
+#number = input('Vvedite chislo: ')
+#random_num = 54
+#lives = 3
+#while number.isnumeric():
+#
+#    if int(number) == random_num:
+#        print('You win! Congrats from Bill Jigsaw!')
+#        break
+#    else:
+#        lives -= 1
+#        print('Ne pravilno ! U vas ostalos ' + str(lives) + ' popitok')
+#        number = input('Vvedite chislo: ')
+#    if lives == 0:
+#        print('U vas ne ostalos popitok. Igra zakonchena!')
+#        break
+
+#                              ===========================================
+#                              ==============  CIKL FOR ==================
+#                              ===========================================
+
+#for i in range(20, 10, -2):
+#    print('Counter: ' + str(i))
+
+#num = int(input('Vvedite chislo: '))
+#for i in range(num):
+#    print('Number: ' + str(i))
+
+#total = 0
+#for i in range(4):
+#    number = float(input('Vvedite chislo: '))
+#    total = number
+#print('Summa vvedennih chisel: ' + str(total))
+
+#n = int(input('Vvedite chislo: '))
+#total = 0
+#for i in range(n):
+#    print('Chislо ', i)
+#    total += i
+#    print('Summa vvedennih chisel: ' + str(total))
+#print('Obshaya summa: ' + str(total))
+
+#======================================================================
+#first_num = int(input('Vvedite pervoe chislo: '))
+#last_num = int(input('Vvedite poslednee chislo: '))
+#sum = 0
+#for i in range(first_num, last_num):
+#    sum += i
+#print('Summa chisel ot ' + str(first_num) + ' do ' + str(last_num) + ' ravna: ' + str(sum))
+#======================================================================
+
+#======================================================================
+#first_number = int(input('Vvedite pervoe chislo: '))
+#last_number = int(input('Vvedite poslednee chislo: '))
+#
+#for i in range(first_number, last_number):
+#    if i % 2 == 0:
+#        print('Chetnoe chislo: ' + str(i))
+#======================================================================
+
+#======================================================================
+#number = int(input('Vvedite chislo: '))
+#count_of_str = int(input('Vvedite kolichestvo strok: '))
+#result = 0
+#for i in range(count_of_str):
+#    result = i * number
+#    print(i, '*', number, '=' , result)
+##======================================================================
+
