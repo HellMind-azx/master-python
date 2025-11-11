bce0f111732e5cda631bc1c9f591b108752a7443
HellMind-azx <nostalgiabook176@gmail.com>
2025-11-11 14:53:53 +0500

    m

---
+++ lessons/set.py
@@ -0,0 +1,16 @@
+#s = 'aaabbbwww'
+#s = set(s)
+#print(s.symmetric_difference('bca'))
+
+#s.update('xyz')
+#print(s)
+#s.remove('a')
+#print(s)
+
+#words = input().strip()
+#s = words.split()
+#x = set(s)
+#print(len(s))
+#print(len(x))
+
+print(set(0,1,2))
--- lessons/while.py
+++ lessons/while.py
@@ -7,7 +7,7 @@
 # total = 0.0
 #
 
-#=================================
+# =================================
 
 # while True:
 #    price = float(input())
@@ -18,63 +18,91 @@
 #    total += price
 # print(total)
 
-#==================================
+# ==================================
 
-#total = 0
+# total = 0
 #
-#while True:
+# while True:
 #    price = float(input('Enter a price (negativa number - break process): '))
-#    
+#
 #    if price < 0:
 #        break
-#    
+#
 #    elif price > 1000:
 #        price *= 0.95
-#        
+#
 #    total += price
-#print('Total price: ', total)
+# print('Total price: ', total)
 
-#=================================
+# =================================
 
-#+price = float(input("Введите цену товара: "))
-#+total = 0
-#+
-#+while price >= 0:  # пока цена не отрицательная
-#+    if price > 1000:
-#+        price *= 0.95
-#+    total += price
-#+    price = float(input("Введите цену товара: "))
-#+
-#+print("Общая стоимость:", total)
+# +price = float(input("Введите цену товара: "))
+# +total = 0
+# +
+# +while price >= 0:  # пока цена не отрицательная
+# +    if price > 1000:
+# +        price *= 0.95
+# +    total += price
+# +    price = float(input("Введите цену товара: "))
+# +
+# +print("Общая стоимость:", total)
 
-count = 0            # количество подходящих кандидатов
-min_height = None    # минимальный рост
-max_height = None    # максимальный рост
-
-while True:
-    data = input("Введите рост претендента (или '!' для окончания): ")
-    if data == "!":
-        break
-
-    height = int(data)
-
-    # учитываем кандидата
-    count += 1
-
-    # если это первый кандидат — задаём минимальный и максимальный рост
-    if min_height is None or max_height is None:
-        min_height = max_height = height
-    else:
-        # обновляем минимальный и максимальный рост
-        if height < min_height:
-            min_height = height
-        if height > max_height:
-            max_height = height
-
+# count = 0            # количество подходящих кандидатов
+# min_height = None    # минимальный рост
+# max_height = None    # максимальный рост
+#
+# while True:
+#    data = input("Введите рост претендента (или '!' для окончания): ")
+#    if data == "!":
+#        break
+#
+#    height = int(data)
+#
+#    # учитываем кандидата
+#    count += 1
+#
+#    # если это первый кандидат — задаём минимальный и максимальный рост
+#    if min_height is None or max_height is None:
+#        min_height = max_height = height
+#    else:
+#        # обновляем минимальный и максимальный рост
+#        if height < min_height:
+#            min_height = height
+#        if height > max_height:
+#            max_height = height
+#
 # вывод результатов
-if count > 0:
-    print(count)
-    print(min_height, max_height)
-else:
-    print("Нет данных о росте")
+# if count > 0:
+#    print(count)
+#    print(min_height, max_height)
+# else:
+#    print("Нет данных о росте")
+
+# ================================
+
+# count = 0
+#
+# while True:
+#    text = input()
+#    count += 1
+#    if text == "Спасибо.":
+#        break
+#
+# print(count)
+
+# ================================
+
+#total = 0
+#count = 0
+#
+#while True:
+#    temp = float(input())
+#    if temp < -300:
+#        break
+#    total += temp
+#    count += 1
+#    
+#average = total / count
+#
+#print(average)
 
