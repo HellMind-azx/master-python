278c11d73e91b296d13f1ea1c153bec483844c7e
HellMind-azx <nostalgiabook176@gmail.com>
2025-11-15 14:59:25 +0500

    cikl for

---
+++ lessons/for.py
@@ -0,0 +1,48 @@
+#total = 0
+#for i in range(3):
+#    price = float(input())
+#    total = total + price
+#print('Сумма введённых чисел равна', total)
+
+#n = int(input())
+#total = 0
+#for i in range(n):
+#    print('Рассматриваем число', i)
+#    total += i
+#    print('Промежуточная сумма равна', total)
+#print('Итоговая сумма всех этих чисел равна', total)
+
+#for i in range(8):
+#    count_of_string = input()+
+#    print(count_of_string, end="")
+
+#sep = input()
+#word1 = input()
+#word2 = input()
+#word3 = input()
+#
+#print(word1, word2, word3, sep=sep)
+
+
+#phrase = input()
+#n = int(input())
+#for i in range(n):
+#    print(phrase)
+
+#n = int(input())
+#for i in range(n+1):
+#    print(i, end=" ")
+
+#n = int(input())
+#for i in range(n):
+#    cube = i ** 3
+#    print(f'Куб числа {i} равен {cube}')
+
+#for i in range(7):
+#    if i == 0:
+#        continue
+#    total = 0
+#    total += i * 2
+#print(total)
+
+
